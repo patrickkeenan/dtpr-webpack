13e3c9bdbd20a71e8902519aa8ddd8c53f0e759d
Patrick Keenan <me@patrickkeenan.me>
2020-06-26 01:19:36 -0400

    responsive

--- src/script.js
+++ src/script.js
@@ -1,4 +1,6 @@
 console.log("jQuery");
+var currentSection;
+var currentAgent;
 $(function () {
   console.log("loaded");
   $(".prototype-loader .btn").click(function () {
@@ -18,6 +20,34 @@ $(function () {
     iFrame.attr("src", iFrame.attr("data-src"));
   });
   const scroller = scrollama();
+  $(".nav-current-section").click(function () {
+    console.log($(currentSection).next(".step"));
+    $("html, body").animate(
+      { scrollTop: $(currentSection).offset().top },
+      1000
+    );
+  });
+  $(".nav-prev-section").click(function () {
+    console.log($(currentSection).next(".step"));
+    $("html, body").animate(
+      { scrollTop: $(currentSection).prev(".step").offset().top },
+      1000
+    );
+  });
+  $(".nav-next-section").click(function () {
+    console.log($(currentSection).next(".step"));
+    $("html, body").animate(
+      { scrollTop: $(currentSection).next(".step").offset().top },
+      1000
+    );
+  });
+  currentAgent = $($(".ai-option.selected .avatar .material-icons")[0]).text();
+  console.log(currentAgent);
+  $(".ai-option").click(function () {
+    $(".ai-option.selected").removeClass("selected");
+    $(this).addClass("selected");
+    currentAgent = $(this).find(".avatar .material-icons").text();
+  });
 
   // setup the instance, pass callback functions
   scroller
@@ -26,6 +56,7 @@ $(function () {
       // progress: true,
     })
     .onStepEnter((response) => {
+      currentSection = response.element;
       console.log(response);
       // if (response.direction == "down") {
       //   $(".nav-container").css("top", -48);
@@ -40,7 +71,6 @@ $(function () {
     .onStepProgress((response) => {
       console.log(response);
     });
-
   // setup resize event
   window.addEventListener("resize", scroller.resize);
 });
